122f08bc37c3b0ffe2704801e6a743b52e4a7ec3
marinam0406 <marinam0406@yandex.ru>
2024-02-27 22:42:37 +0300

    add vacancies.py

---
+++ src/vacancies.py
@@ -0,0 +1,22 @@
+class Vacancy:
+    def __init__(self, name, page):
+        """Создание экземпляра класса Vacancy"""
+        self.__name = name
+        self.__page = page
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def page(self):
+        return self.__page
+
+    def __str__(self):
+        return f"{self.__name}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.__name, self.__page})"
+
+
+
