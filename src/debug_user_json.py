64c69af7508524f639c619cf8279e51ab23330c5
marinam0406 <marinam0406@yandex.ru>
2024-02-27 22:39:41 +0300

    add debug_user_json.py

---
+++ src/debug_user_json.py
@@ -0,0 +1,31 @@
+from src.user_forms import UserForm
+
+
+class DebugUserJson(UserForm):
+    payment = None
+    city = None
+
+    def user_input_int(self):
+        """
+        Проверка на наличие ошибок ввода
+        """
+        self.payment = input("Введите минимальную заработную плату: ")
+        if self.payment.isalpha():
+            raise ValueError("Неверно указана зарплата. Убедитесь, что указано число.")
+        if self.payment == "":
+            self.payment = 0
+        return int(self.payment)
+
+    def user_input_str(self):
+        """
+        Проверка на наличие ошибок ввода
+        """
+        self.city = input("Введите желаемый город: ").title()
+        if self.city.isdigit():
+            raise TypeError("Город не может быть числом")
+        return self.city
+
+
+if __name__ == '__main__':
+    r = DebugUserJson()
+    print(r.user_input_str())
